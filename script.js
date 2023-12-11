16be0fa093b7cb95eb5df50170fc17ee4197bcdd
Abdul Gaffar <abdulgaffarefti66@gmail.com>
2023-12-11 17:07:25 +0600

    first landingpage

---
+++ script.js
@@ -0,0 +1,21 @@
+const bar=document.querySelector('#bar')
+const cross=document.querySelector('#cross')
+const navlink = document.querySelector('.navbars')
+const heroField=document.querySelector('.hero-container')
+
+bar.addEventListener('click',()=>{
+    navlink.classList.remove('deactive');
+    bar.classList.add('deactive');
+    cross.classList.remove('deactive','visibility');
+})
+cross.addEventListener('click',()=>{
+    navlink.classList.remove('deactive');
+    cross.classList.add('deactive');
+    bar.classList.remove('deactive');
+    navlink.classList.add('deactive');
+})
+heroField.addEventListener('click',()=>{
+    navlink.classList.add('deactive');
+    bar.classList.remove('deactive');
+    cross.classList.add('deactive');
+})
